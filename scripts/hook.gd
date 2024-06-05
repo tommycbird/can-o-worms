dc2ecc03c7318ad78a12bc28c68f7d40ccfd7fac
tommycbird <tommycbird@gmail.com>
2024-06-04 22:37:13 -0600

    basic gameplay

---
+++ scripts/hook.gd
@@ -0,0 +1,43 @@
+extends Sprite2D
+
+# min/max hook pos
+const MIN_Y = -550
+const MAX_Y = 1100
+
+# reeling speed
+const HOOK_SPEED = 500
+
+# move the hook up
+var reel = true
+
+func _ready():
+	# enable input processing
+	set_process_input(true)
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			reel = false
+		else:
+			reel = true
+
+func _process(delta):
+	# move the hook
+	if reel:
+		reel_in(delta)
+	else:
+		reel_out(delta)
+
+func reel_out(delta):
+	# move the hook down
+	if position.y < MAX_Y:
+		position.y += HOOK_SPEED * delta
+		if position.y > MAX_Y:
+			position.y = MAX_Y
+
+func reel_in(delta):
+	# move the hook up
+	if position.y > MIN_Y:
+		position.y -= HOOK_SPEED * delta
+		if position.y < MIN_Y:
+			position.y = MIN_Y
